5dd195a6c632ca0ea0f8ed5f2f43e6c0854de000
Richa Mittal <richa.mittal@smartbear.com>
2016-10-17 11:24:37 +0200

    Multiple tests with multiple configs

--- src/test/java/FirstSeleniumCBTTest.java
+++ src/test/java/FirstSeleniumCBTTest.java
@@ -2,16 +2,20 @@
  * Created by richa.mittal on 2016-10-14.
  */
 
-
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -21,45 +25,47 @@ import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
-import static org.junit.Assert.*;
+@RunWith(Parameterized.class)
 public class FirstSeleniumCBTTest {
-    static String username = "richa.mittal%40smartbear.com"; // Your username
-    static String authkey = "u0caf2eae0f3bbdd";  // Your authkey
+    String username = "richa.mittal%40smartbear.com"; // Your username
+    String authkey = "u0caf2eae0f3bbdd";  // Your authkey
     String testScore = "unset";
     RemoteWebDriver driver;
+
+    private String os;
+    private String browser;
+    private String screenResolution;
 
     @Before
     public void setUp() throws MalformedURLException {
 
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("name", "ApiSpy Test");
-        caps.setCapability("build", "1.0");
-        caps.setCapability("browser_api_name", "Chrome53x64");
-        caps.setCapability("os_api_name", "Win7x64-C1");
-        caps.setCapability("screen_resolution", "1024x768");
-        caps.setCapability("record_video", "false");
-        caps.setCapability("record_network", "false");
-
-        driver = new RemoteWebDriver(new URL("http://" + username + ":" + authkey + "@hub.crossbrowsertesting.com:80/wd/hub"), caps);
-        System.out.println(driver.getSessionId());
-
-        String apispy_username = "spy";
-        String apispy_password = "dontspyhere";
-        String baseUrl = "http://"+apispy_username+":"+apispy_password+"@apispy.io:3000";
-        driver.get(baseUrl);
-        System.out.println("Loading Url");
-        driver.get("http://spy:dontspyhere@apispy.io:3000");
+        DesiredCapabilities caps = setCapabilities();
+        openAPISpyOnCBT(caps);
     }
     @After
     public void tearDown(){
           // quit the driver
-            driver.quit();
+           driver.quit();
+    }
+
+    public FirstSeleniumCBTTest(String os, String browser, String screenResolution){
+        this.os = os;
+        this.browser = browser;
+        this.screenResolution = screenResolution;
+    }
+
+    @Parameterized.Parameters
+    public static Collection environments(){
+        return Arrays.asList(new Object[][]{
+                { "Win7x64-C1", "Chrome53x64", "1024x768"  },
+                { "Win7x64-C1", "FF46x64", "1024x768"  },
+                { "Win10", "Edge20", "1024x768"  },
+                { "Mac10.12", "Safari10", "1024x768"  }
+        });
     }
 
     @Test
     public void MethodIsDisplayedTest() throws Exception {
-
-        FirstSeleniumCBTTest myTest = new FirstSeleniumCBTTest();
 
         try {
 
@@ -68,79 +74,90 @@ public class FirstSeleniumCBTTest {
 
             // if we get to this point, then all the assertions have passed
             // that means that we can set the score to pass in our system
-             myTest.testScore = "pass";
+             testScore = "pass";
         } catch (AssertionError ae) {
 
             // if we have an assertion error, take a snapshot of where the test fails
             // and set the score to "fail"
-            String snapshotHash = myTest.takeSnapshot(driver.getSessionId().toString());
-            myTest.setDescription(driver.getSessionId().toString(), snapshotHash, ae.toString());
-            myTest.testScore = "fail";
+            String snapshotHash = takeSnapshot(driver.getSessionId().toString());
+            setDescription(driver.getSessionId().toString(), snapshotHash, ae.toString());
+            testScore = "fail";
         } finally {
 
-            System.out.println("Test complete: " + myTest.testScore);
+            System.out.println("Test complete: " + testScore);
 
             // here we make an api call to actually send the score
-            myTest.setScore(driver.getSessionId().toString(), myTest.testScore);
+            setScore(driver.getSessionId().toString(), testScore);
         }
     }
 
-    @Test
-    public void URLIsDisplayedTest() throws Exception {
 
-        FirstSeleniumCBTTest myTest = new FirstSeleniumCBTTest();
+      @Test
+      public void URLIsDisplayedTest() throws Exception {
+          try {
+              //test if the URL is displayed
+              Assert.assertTrue(driver.findElement(By.id("req-url")).isDisplayed());
+              // if we get to this point, then all the assertions have passed
+              // that means that we can set the score to pass in our system
+              testScore = "pass";
+          } catch (AssertionError ae) {
+              // if we have an assertion error, take a snapshot of where the test fails
+              // and set the score to "fail"
+              String snapshotHash = takeSnapshot(driver.getSessionId().toString());
+              setDescription(driver.getSessionId().toString(), snapshotHash, ae.toString());
+              testScore = "fail";
+          } finally {
+              System.out.println("Test complete: " + testScore);
+              // here we make an api call to actually send the score
+              setScore(driver.getSessionId().toString(), testScore);
+          }
+      }
 
-        try {
-            //test if the URL is displayed
-            Assert.assertTrue(driver.findElement(By.id("req-url")).isDisplayed());
-            //test if the send button is displayed
-            //Assert.assertTrue(driver.findElement(By.id("sendButton")).isDisplayed());
+      @Test
+      public void SendIsDisplayedTest() throws Exception {
+         try {
+              //test if the send button is displayed
+              Assert.assertTrue(driver.findElement(By.id("sendButton")).isDisplayed());
+              // if we get to this point, then all the assertions have passed
+              // that means that we can set the score to pass in our system
+              testScore = "pass";
+         } catch (AssertionError ae) {
 
-            // if we get to this point, then all the assertions have passed
-            // that means that we can set the score to pass in our system
-            myTest.testScore = "pass";
-        } catch (AssertionError ae) {
+              // if we have an assertion error, take a snapshot of where the test fails
+              // and set the score to "fail"
+              String snapshotHash = takeSnapshot(driver.getSessionId().toString());
+              setDescription(driver.getSessionId().toString(), snapshotHash, ae.toString());
+              testScore = "fail";
+         } finally {
 
-            // if we have an assertion error, take a snapshot of where the test fails
-            // and set the score to "fail"
-            String snapshotHash = myTest.takeSnapshot(driver.getSessionId().toString());
-            myTest.setDescription(driver.getSessionId().toString(), snapshotHash, ae.toString());
-            myTest.testScore = "fail";
-        } finally {
+              System.out.println("Test complete: " + testScore);
 
-            System.out.println("Test complete: " + myTest.testScore);
+              // here we make an api call to actually send the score
+              setScore(driver.getSessionId().toString(), testScore);
+         }
+      }
 
-            // here we make an api call to actually send the score
-            myTest.setScore(driver.getSessionId().toString(), myTest.testScore);
-        }
+    public DesiredCapabilities setCapabilities (){
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("name", "ApiSpy Test");
+        caps.setCapability("build", "1.0");
+        caps.setCapability("os_api_name", os);
+        caps.setCapability("browser_api_name", browser);
+        caps.setCapability("screen_resolution", screenResolution);
+        caps.setCapability("record_video", "true");
+        caps.setCapability("record_network", "false");
+        return caps;
     }
 
-    @Test
-    public void SendIsDisplayedTest() throws Exception {
-
-        FirstSeleniumCBTTest myTest = new FirstSeleniumCBTTest();
-
-        try {
-            //test if the send button is displayed
-            Assert.assertTrue(driver.findElement(By.id("sendButton")).isDisplayed());
-
-            // if we get to this point, then all the assertions have passed
-            // that means that we can set the score to pass in our system
-            myTest.testScore = "pass";
-        } catch (AssertionError ae) {
-
-            // if we have an assertion error, take a snapshot of where the test fails
-            // and set the score to "fail"
-            String snapshotHash = myTest.takeSnapshot(driver.getSessionId().toString());
-            myTest.setDescription(driver.getSessionId().toString(), snapshotHash, ae.toString());
-            myTest.testScore = "fail";
-        } finally {
-
-            System.out.println("Test complete: " + myTest.testScore);
-
-            // here we make an api call to actually send the score
-            myTest.setScore(driver.getSessionId().toString(), myTest.testScore);
-        }
+    public void openAPISpyOnCBT(DesiredCapabilities caps) throws MalformedURLException {
+        driver = new RemoteWebDriver(new URL("http://" + username + ":" + authkey + "@hub.crossbrowsertesting.com:80/wd/hub"), caps);
+        System.out.println(driver.getSessionId());
+        String apispy_username = "spy";
+        String apispy_password = "dontspyhere";
+        String baseUrl = "http://"+apispy_username+":"+apispy_password+"@apispy.io:3000";
+        driver.get(baseUrl);
+        System.out.println("Loading Url");
+        driver.get("http://spy:dontspyhere@apispy.io:3000");
     }
 
     public JsonNode setScore(String seleniumTestId, String score) throws UnirestException {
